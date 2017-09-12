df767af6a43c42f5b9d0704d3ea4cf9e4bb41829
john <john@voilamediagroup.com>
2017-09-12 14:42:16 -0500

    first commit

---
+++ js/requestBar.js
@@ -0,0 +1,32 @@
+//Make Query
+//Store Data
+//API REQUEST - Beanie demo 008421043064
+//https://api.upcitemdb.com/prod/trial/lookup?upc=
+
+upc = '008421043064';
+//post the request
+function searchBarCode(upc){
+	$.ajax({
+	    type: 'GET',
+	    url: 'https://api.upcitemdb.com/prod/trial/lookup',
+	    data: "upc=" + upc,
+	    success: function (resp) {
+	        //console.log("Debug:" + resp);
+	        console.log("Debug: " + JSON.stringify(resp, null, 4));
+	        
+	        if(resp.success == true){//now lets mark the columns that we saved.
+		        
+		    }
+		    $.mobile.loading("hide");
+		},
+		error: function(e){
+			//console.log(e);
+			$.mobile.loading("hide");
+			console.log("Debug: error" + JSON.stringify(e, null, 4));
+			
+			$(".print-message").removeClass('bi-invisible');
+			$(".print-message").addClass("bi-ctn-err");
+			$(".print-message").html("Error syncing.");
+		}
+	});
+}
